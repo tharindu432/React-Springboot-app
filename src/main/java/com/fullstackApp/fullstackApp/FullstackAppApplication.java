f3248ce221de67ab4e296c761a835ae119f53c40
tharindu432 <tharinduchaturanga432@gmail.com>
2023-08-28 11:47:41 +0530

    initial commit

---
+++ src/main/java/com/fullstackApp/fullstackApp/FullstackAppApplication.java
@@ -0,0 +1,14 @@
+package com.fullstackApp.fullstackApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackAppApplication.class, args);
+		System.out.println("hello world!");
+	}
+
+}
