152d37776ed8370babb8d2dc46d14a5e50eef5f2
tharindu432 <tharinduchaturanga432@gmail.com>
2023-09-04 16:09:16 +0530

    database connected successfully

--- src/main/java/com/fullstackApp/fullstackApp/Controller/EmployeeController.java
+++ src/main/java/com/fullstackApp/fullstackApp/Controller/EmployeeController.java
@@ -1,12 +1,28 @@
 package com.fullstackApp.fullstackApp.Controller;
 
+import com.fullstackApp.fullstackApp.Model.Employee;
+import com.fullstackApp.fullstackApp.Repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/")
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+
+// get all employees
 @GetMapping("/employees")
-    public String getEmployee(){
-        return "hello";
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
 
 
     }
